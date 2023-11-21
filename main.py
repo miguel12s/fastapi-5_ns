161f150ccc64552b33ccf16b7cfbf1f7c7f647bf
miguel12s <87570624+miguel12s@users.noreply.github.com>
2023-11-20 20:56:15 -0500

    commit

--- main.py
+++ main.py
@@ -1,7 +1,46 @@
-from fastapi import FastAPI
 
-app = FastAPI()
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+app= FastAPI()
 
-@app.get("/")
-async def root():
-    return {"greeting": "Hello, World!", "message": "Welcome to FastAPI!"}
+class EmailItem(BaseModel):
+    subject: str
+    message: str
+    to_email: str
+
+@app.post("/send-email/")
+def send_email(item: EmailItem):
+    # Configura la conexión SMTP para Gmail
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+    smtp_username = "miguelaspet@gmail.com"  # Reemplaza con tu dirección de correo electrónico de Gmail
+    smtp_password = "xrldipesfbgzmbag"  # Reemplaza con tu contraseña de aplicación generada
+
+    # Configura el mensaje de correo
+    msg = MIMEMultipart()
+    msg["From"] = smtp_username
+    msg["To"] = item.to_email
+    msg["Subject"] = item.subject
+
+    # Agrega el cuerpo del mensaje
+    msg.attach(MIMEText(item.message, "plain"))
+
+    try:
+        # Inicia la conexión SMTP
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            # Inicia la sesión SMTP
+            server.starttls()
+
+            # Inicia sesión con las credenciales
+            server.login(smtp_username, smtp_password)
+
+            # Envía el correo
+            server.sendmail(smtp_username, item.to_email, msg.as_string())
+
+        return {"message": "Email sent successfully"}
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error sending email: {str(e)}")
